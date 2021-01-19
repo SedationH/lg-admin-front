02900b0e239ed609dc4a5309130ad8d7e3f123fb
SedationH <sedationh@gmail.com>
2021-01-19 12:02:31 +0800

    ⚡️ import element-plus some stuffs of prettier

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// import installElementPlus from './plugins/element'
+
+import ElementPlus from 'element-plus'
+import '@/assets/styles/element-variables.scss'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
 
 createApp(App)
+  .use(ElementPlus, { locale })
   .use(store)
   .use(router)
-  .mount("#app");
+  .mount('#app')
+
+// installElementPlus(app)
