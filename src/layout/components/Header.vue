3b128facd01d4ab34e857b9e2b41b96654907e59
SedationH <sedationh@gmail.com>
2021-01-21 22:32:31 +0800

    🔥 handle exit logic

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -14,13 +14,61 @@
         >promotion detail</el-breadcrumb-item
       >
     </el-breadcrumb>
+
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        <el-avatar
+          shape="square"
+          size="large"
+          :src="
+            require('@/assets/images/default-avatar.png')
+          "
+        />
+        <i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>
+            SedationH
+          </el-dropdown-item>
+          <el-dropdown-item @click="onExit">
+            退出
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { mapGetters, mapMutations } from 'vuex'
+
+export default defineComponent({
+  computed: {
+    ...mapGetters('user', ['userInfo'])
+  },
+  methods: {
+    ...mapMutations('user', ['setUserLoginInfo']),
+    onExit() {
+      this.setUserLoginInfo(null)
+      this.$router.push('/login')
+    }
+  }
+})
+</script>
 
 <style lang="scss" scoped>
 .header-container {
   height: 100%;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  margin: 0 30px;
+
+  .el-dropdown-link {
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
